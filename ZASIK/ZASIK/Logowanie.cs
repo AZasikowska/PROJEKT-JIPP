91a1052d8f524d972ad3592506182aac0d5b472b
LAPTOP-CUPIGH75\Aga <azasikowska@gmail.com>
2017-06-03 07:29:02 +0200

    Oddane

--- ZASIK/ZASIK/Logowanie.cs
+++ ZASIK/ZASIK/Logowanie.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -28,48 +28,39 @@ namespace ZASIK
 
         private void button2_Click(object sender, EventArgs e) 
         {
+            label3.Text = "";
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
 
                 SqlCommand command = new SqlCommand();
-                SqlDataReader reader;
 
-                command.CommandText = "SELECT * from  [KASETY_404_20].[Z404_20].[Users] ;";
+
+                command.CommandText = "SELECT count(*) from  [KASETY_404_20].[Z404_20].[Users] where login = '" + textBox1.Text + "' AND haslo ='" + textBox2.Text + "';";
                 command.CommandType = CommandType.Text;
                 command.Connection = connection;
 
                 connection.Open();
-                reader = command.ExecuteReader();
-                if (connection.State == ConnectionState.Open)
+                int isCorrect = (int)command.ExecuteScalar();
+                connection.Close();
+                if (isCorrect == 1)
                 {
-
-                    String login;
-                    String haslo;
-                    while (reader.Read())
-                    {
-                        login = reader.GetValue(1).ToString();
-                        haslo = reader.GetValue(2).ToString();
-
-                        if (this.textBox1.Text == login && this.textBox2.Text == haslo)
-                        {
-                            progressBar1.Visible = true;
-                            label4.Text = string.Format("Logowanie..");
-                            timer1.Start();
-                        }
-
-                        else
-                        {
-                            label3.Text = "Nieprawidłowy login lub hasło!";
-                        }
-                    }
+                    progressBar1.Visible = true;
+                    label4.Text = string.Format("Logowanie..");
+                    timer1.Start();
+                }
+                else
+                {
+                    label3.Text = "Nieprawidłowy login lub hasło!";
+                    textBox1.Enabled = true;
+                    textBox2.Enabled = true;
+                }
 
 
                 }
             }
-        }
-
-
-
+        
 
         private void timer1_Tick(object sender, EventArgs e)
         {
